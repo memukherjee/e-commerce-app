33742dbd3ddb34fa4f254c4a984d4bdee0bb63e1
unknown <anurag.gomia@gmail.com>
2022-11-04 21:10:36 +0530

    forgot,route

--- backend/src/main/java/com/ecommerce/Controller/UserController.java
+++ backend/src/main/java/com/ecommerce/Controller/UserController.java
@@ -16,44 +16,91 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.Entity.UserData;
+import com.ecommerce.Entity.objholder;
 import com.ecommerce.Repository.UserRepo;
 import com.ecommerce.Service.UserService;
 
 @RestController
 @RequestMapping("/ecommerce/user")
 public class UserController {
+	String email; // forgotpass_link
+	String otp;
+
+	Vector forgot = new Vector(); // forgotpass_link
 
 	@Autowired
 	UserService userService;
 
+//                                     *************************signup module**************************** 
 	@PostMapping("/signup")
-	public UserData signup(@Valid @RequestBody UserData userData) {
-		UserData user = userService.saveDataTODB(userData);
+	public Vector signup(@Valid @RequestBody UserData userData) {
+		Vector user = new Vector();
+		user = userService.saveDataTODB(userData);
 		return user;
 	}
 
+//                                 **************************details of user***********************
 	@GetMapping("getUserDetails/{id}")
 	public UserData getUserDetails(@PathVariable String id) {
 		UserData user = userService.getsUserDetailsFromDB(id);
 		return user;
 	}
 
-	@PostMapping("/login/{email}/{pass}")
-	public Vector MatchData(@PathVariable String email, @PathVariable String pass) {
-		System.out.println("login controller email and pass=" + email + " : " + pass);
-		UserData user = userService.findByEmail(email, pass);
-		Vector v=new Vector();
+	// ************************* login module*************************
+	
+
+	@PostMapping("/login")
+	public Vector MatchData(@RequestBody objholder obj){
+		System.out.println("login controller email and pass=" + obj.email + " : " + obj.pass);
+		UserData user = userService.findByEmail(obj.email,obj.pass);
+		Vector v = new Vector();
 		if (user != null) {
 			v.add(user);
-		}else
-		v.add(new ResponseEntity<String>(HttpStatus.UNAUTHORIZED));
+		} else
+			v.add(new ResponseEntity<String>(HttpStatus.UNAUTHORIZED));
 		return v;
-		
 
 	}
-//	 
+
+	//                                   ************************** Forgot password module **********************************
+
+	@PostMapping("/forgotpass")
+	public Vector ForgotPass(@RequestBody objholder str) {
+		this.email = str.email;
+		System.out.println("forgotpass controller email=" + str.email);
+		forgot = userService.findByEmail(str.email);
+
+		return forgot;
+
+	}
+
+	@PostMapping("/otp")
+	public Vector otp(@RequestBody objholder str) {
+		Vector votp = new Vector();
+		this.otp = str.otp;
+		if (forgot.contains(otp)) {
+			System.out.println("matched");
+			votp.add(new ResponseEntity<String>(HttpStatus.ACCEPTED));
+		} else {
+			System.out.println("not matched");
+			votp.add(new ResponseEntity<String>(HttpStatus.NOT_ACCEPTABLE));
+		}
+		return votp;
+
+	}
+
+	@PostMapping("/reset")
+	public Vector reset(@RequestBody objholder str) {
+		System.out.println("reset speaking");
+		System.out.println(str.pass);
+		System.out.println(email);
+		Vector user = userService.findByEmailreset(email, str.pass);
+		return user;
+
+	}
 
 }

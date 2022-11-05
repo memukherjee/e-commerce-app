dd684921468c14efcd0e23999fb3efa1ba4ee091
Akash Mukherjee <akash3.11.2000@gmail.com>
2022-11-05 18:27:14 +0530

    api route names resolved

--- backend/src/main/java/com/ecommerce/Controller/UserController.java
+++ backend/src/main/java/com/ecommerce/Controller/UserController.java
@@ -7,25 +7,23 @@ import java.util.Vector;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.Entity.UserData;
 import com.ecommerce.Entity.objholder;
-import com.ecommerce.Repository.UserRepo;
 import com.ecommerce.Service.UserService;
 
 @RestController
-@RequestMapping("/ecommerce/user")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
 public class UserController {
 	String email; // forgotpass_link
 	String otp;

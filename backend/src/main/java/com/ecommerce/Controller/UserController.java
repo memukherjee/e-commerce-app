33abbe198decee4845532a1827f994bf0f6b1c36
unknown <anurag.gomia@gmail.com>
2022-11-11 21:52:42 +0530

    updated response

--- backend/src/main/java/com/ecommerce/Controller/UserController.java
+++ backend/src/main/java/com/ecommerce/Controller/UserController.java
@@ -7,6 +7,8 @@ import java.util.Vector;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.Entity.UserData;
 import com.ecommerce.Entity.objholder;
+import com.ecommerce.Repository.UserRepo;
 import com.ecommerce.Service.UserService;
 
 @RestController
@@ -52,52 +56,52 @@ public class UserController {
 	
 
 	@PostMapping("/login")
-	public Vector MatchData(@RequestBody objholder obj){
+	public ResponseEntity<Object> MatchData(@RequestBody objholder obj){
 		System.out.println("login controller email and pass=" + obj.email + " : " + obj.pass);
 		UserData user = userService.findByEmail(obj.email,obj.pass);
 		Vector v = new Vector();
 		if (user != null) {
-			v.add(user);
+			//v.add(user);
+			return new ResponseEntity<>(user, HttpStatus.OK);
 		} else
-			v.add(new ResponseEntity<String>(HttpStatus.UNAUTHORIZED));
-		return v;
-
+			return new ResponseEntity<>("User Not Found", HttpStatus.NOT_FOUND);
+		
 	}
 
 	//                                   ************************** Forgot password module **********************************
 
 	@PostMapping("/forgotpass")
-	public Object ForgotPass(@RequestBody objholder str) {
+	public ResponseEntity<String> ForgotPass(@RequestBody objholder str) {
 		this.email = str.email;
 		System.out.println("forgotpass controller email=" + str.email);
 		forgot = userService.findByEmail(str.email);
 
-		return forgot.lastElement();
+		return new ResponseEntity<>("Email Verified", HttpStatus.OK);
 
 	}
 
 	@PostMapping("/otp")
-	public Object otp(@RequestBody objholder str) {
+	public ResponseEntity<String> otp(@RequestBody objholder str) {
 		Vector votp = new Vector();
 		this.otp = str.otp;
 		if (forgot.contains(otp)) {
 			System.out.println("matched");
-			votp.add(new ResponseEntity<String>(HttpStatus.ACCEPTED));
+			return new ResponseEntity<>("OTP Verified", HttpStatus.OK);
 		} else {
 			System.out.println("not matched");
-			votp.add(new ResponseEntity<String>(HttpStatus.NOT_ACCEPTABLE));
+			return new ResponseEntity<>("OTP Incorrect", HttpStatus.NOT_FOUND);
 		}
-		return votp.lastElement();
+		
 
 	}
 
 	@PostMapping("/reset")
-	public Object reset(@RequestBody objholder str) {
+	public ResponseEntity<String> reset(@RequestBody objholder str) {
 		System.out.println("reset speaking");
 		System.out.println(str.pass);
 		System.out.println(email);
-		Vector user = userService.findByEmailreset(email, str.pass);
-		return user.lastElement();
+		ResponseEntity<String> user = userService.findByEmailreset(email, str.pass);
+		return user;
 
 	}
 

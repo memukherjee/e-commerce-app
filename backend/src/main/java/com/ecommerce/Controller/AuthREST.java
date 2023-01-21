74fd78d9c5d3329159551aae2fdb27967376c701
unknown <anurag.gomia@gmail.com>
2023-01-21 18:56:34 +0530

    get

--- backend/src/main/java/com/ecommerce/Controller/AuthREST.java
+++ backend/src/main/java/com/ecommerce/Controller/AuthREST.java
@@ -25,6 +25,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;

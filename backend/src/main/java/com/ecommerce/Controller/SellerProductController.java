b29a93f74f62f44202901807fa436d32b4f1d048
MILI BISWAS <biswasmili2001@gmail.com>
2023-01-29 18:09:24 +0530

    changes

--- backend/src/main/java/com/ecommerce/Controller/SellerProductController.java
+++ backend/src/main/java/com/ecommerce/Controller/SellerProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.ecommerce.Service.SellerProductService;
 import com.ecommerce.jwt.SellerTokenValidator;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/seller")
 public class SellerProductController {
 

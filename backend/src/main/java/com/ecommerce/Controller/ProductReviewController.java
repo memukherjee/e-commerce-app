1f2f921408b6106aa05ea6e680dc889d22d73608
unknown <anurag.gomia@gmail.com>
2023-02-20 13:19:42 +0530

    Minor change

--- backend/src/main/java/com/ecommerce/Controller/ProductReviewController.java
+++ backend/src/main/java/com/ecommerce/Controller/ProductReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -42,9 +43,9 @@ public class ProductReviewController {
 		return new ResponseEntity<>(productReviewRepository.save(productReview),HttpStatus.OK);
 		
 	}
-	@GetMapping("/getproductReview")
-	public ResponseEntity<?> productReview(@RequestBody ProductReviewDTO obj){
-		return new ResponseEntity<>(productReviewRepository.findAllByProductId(obj.getProductId()),HttpStatus.OK);
+	@GetMapping("/getproductReview/{productId}")
+	public ResponseEntity<?> productReview(@PathVariable String productId){
+		return new ResponseEntity<>(productReviewRepository.findAllByProductId(productId),HttpStatus.OK);
 	}
 	
 	@GetMapping("/averageStar")

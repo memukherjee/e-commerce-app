b6066b5431ab9aa4ad911e9ee6ea5bdb178ad9d2
Akash Mukherjee <akash3.11.2000@gmail.com>
2023-03-13 21:01:25 +0530

    filter produts is ready

--- backend/src/main/java/com/ecommerce/Service/UserProductService.java
+++ backend/src/main/java/com/ecommerce/Service/UserProductService.java
@@ -1,19 +1,13 @@
 package com.ecommerce.Service;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
 import com.ecommerce.Entity.Product;
 import com.ecommerce.Repository.UserProductRepository;
 
@@ -52,17 +46,17 @@ public class UserProductService {
         PageRequest thirdPageRequest = PageRequest.of(0, 6, Sort.Direction.DESC, "product_sold");
         return (List<Product>) repository.findAll(thirdPageRequest).getContent();
     }
-    
+
     public List<Product> findPopularityByProducts2() {
         PageRequest thirdPageRequest = PageRequest.of(0, 6, Sort.Direction.ASC, "product_sold");
         return (List<Product>) repository.findAll(thirdPageRequest).getContent();
     }
-    
+
     public List<Product> SortPriceByDesc() {
         PageRequest thirdPageRequest = PageRequest.of(0, 6, Sort.Direction.DESC, "discountPrice");
         return (List<Product>) repository.findAll(thirdPageRequest).getContent();
     }
-    
+
     public List<Product> SortPriceByAsc() {
         PageRequest thirdPageRequest = PageRequest.of(0, 6, Sort.Direction.ASC, "discountPrice");
         return (List<Product>) repository.findAll(thirdPageRequest).getContent();
@@ -72,14 +66,13 @@ public class UserProductService {
         return repository.getProductByName(product_name);
 
     }
-    
-    public List<Product> getProductByMaxMin(double max,double min){
-    	return repository.getProductByMaxMin(max, min);
+
+    public List<Product> filterProducts(double max, double min) {
+        return repository.filterProducts(max, min);
     }
 
-	public List<Product> getProductByMaxMinAndCategory(double parseDouble, double parseDouble2,String category) {
-		// TODO Auto-generated method stub
-		return repository.getProductByMaxMinAndCategory(parseDouble,parseDouble2, category);
-	}
+    public List<Product> filterProducts(double parseDouble, double parseDouble2, String category) {
+        return repository.filterProducts(parseDouble, parseDouble2, category);
+    }
 
 }
